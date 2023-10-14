23ed09596e99df28e7aec2082bd7e75d0fa2697b
jonathan person <107312207+Jaypee93@users.noreply.github.com>
2023-10-14 15:02:38 +0300

    jonathan card done

--- src/components/Jonathan.jsx
+++ src/components/Jonathan.jsx
@@ -28,10 +28,4 @@ export default function Jonathan() {
   );
 }
 
-//  Jonathan Walters
-// Verified Graduate
 
-// The team was very supportive and kept me motivated
-
-// “ I started as a total newbie with virtually no coding skills. I now work as a mobile engineer
-// for a big company. This was one of the best investments I’ve made in myself. ”
