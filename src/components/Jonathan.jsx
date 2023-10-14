1ec88c8c85a8985040033b2c1a0398d5874bf261
jonathan person <107312207+Jaypee93@users.noreply.github.com>
2023-10-14 14:53:30 +0300

    Jonathan Walters card done. Added some css to index.css

---
+++ src/components/Jonathan.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "./Jonathan.css";
+import jonathanImg from "../assets/image-jonathan.jpg";
+
+export default function Jonathan() {
+  return (
+    <div className="jonathan-container">
+      <div className="jonathan-about">
+        <img src={jonathanImg} alt="jonathan" className="jonathan-img" />
+        <div className="jonathan-intro">
+          <p>Jonathan Walters</p>
+          <p>
+            <small>Verified Graduate</small>
+          </p>
+        </div>
+      </div>
+      <div className="jonathan-comment">
+        <h1>The team was very supportive and kept me motivated</h1>
+      </div>
+      <div className="jonathan-quote">
+        <h3>
+          “ I started as a total newbie with virtually no coding skills. I now
+          work as a mobile engineer for a big company. This was one of the best
+          investments I’ve made in myself. ”
+        </h3>
+      </div>
+    </div>
+  );
+}
+
+//  Jonathan Walters
+// Verified Graduate
+
+// The team was very supportive and kept me motivated
+
+// “ I started as a total newbie with virtually no coding skills. I now work as a mobile engineer
+// for a big company. This was one of the best investments I’ve made in myself. ”
